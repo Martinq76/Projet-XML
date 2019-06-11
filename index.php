3c77dc5b95249326eb472c4f418b9ff27c5393a5
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-11 11:10:54 +0200

    XML OK

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$XmlSecurite = "https://www.01net.com/rss/actualites/securite/";
+$XmlApplisLogiciels = "https://www.01net.com/rss/actualites/applis-logiciels/";
+$XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
     <head>
@@ -6,7 +11,15 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
     </head>
     <body>
-
+        <?php
+        if (file_exists($XmlSecurite) && file_exists($XmlApplisLogiciels) && file_exists($XmlTechnos)):
+        $RssSecurite = simplexml_load_file($XmlSecurite);
+        $RssApplisLogiciels = simplexml_load_file($XmlApplisLogiciels);
+        $RssTechnos = simplexml_load_file($XmlTechnos);
+        else:
+            echo 'Erreur lors du chargement des fichiers XML';
+        endif;
+        ?>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script src="script.js"></script>
