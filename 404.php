59e03906c836b9475dceee5f4e2e5b9e95ba6a09
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-11 22:11:45 +0200

    Ajout erreur 404 et début routes

--- 404.php
+++ 404.php
@@ -1,3 +1,16 @@
- <?php
-echo 'Erreur, cette page n’existe pas !';
-?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+        <title>Erreur 404</title>
+    </head>
+    <body>
+        <h1>Erreur 404</h1>
+        <p>Désolé mais l'url n'est pas valide...</p>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script src="assets/js/script.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $_SESSION['articleNumber'];
 $_SESSION['choiceColor'];
 
 if (isset($_GET['lastName']) && isset($_GET['firstName']) && isset($_GET['choiceColor']) && isset($_GET['articleNumber'])) {
+    $_GET = array_map('strip_tags',$_GET);
     $loginUtili = $_GET['lastName'];
     $mdpUtili = $_GET['firstName'];
     $choiceColor = $_GET['choiceColor'];
@@ -26,7 +27,7 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
 
 <head>
     <meta charset="UTF-8" />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
 </head>
@@ -42,26 +43,129 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
             <a href="#!" class="brand-logo toto">Logoo</a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
-                <li><a href="sass.html">Sécurité</a></li>
-                <li><a href="badges.html">Application</a></li>
-                <li><a href="collapsible.html">Téchno</a></li>
+                <li><a href="securite">Sécurité</a></li>
+                <li><a href="applis">Application</a></li>
+                <li><a href="technos">Technos</a></li>
                 <li><?= $_GET['firstName'] ?></li>
-                <li><a href="settings.html"><i class="material-icons"> settings</i></a></li>
+                <li><a href="deconnexion">Déconnexion</a></li>
+                <li><a href="parametres"><i class="material-icons"> settings</i></a></li>
             </ul>
         </div>
     </nav>
     <ul class="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Sécurité</a></li>
-        <li><a href="badges.html">Application</a></li>
-        <li><a href="collapsible.html">Téchno</a></li>
-        <li><a href="collapsible.html">se connecter</a></li>
+        <li><a href="securite">Sécurité</a></li>
+        <li><a href="applis">Application</a></li>
+        <li><a href="technos">Technos</a></li>
+        <li><?= $_GET['firstName'] ?></li>
+        <li><a href="deconnexion">Déconnexion</a></li>
+        <li><a href="parametres"><i class="material-icons"> settings</i></a></li>
     </ul>
 
+    <?php
+    if (isset($_GET['page']) && $_GET['page'] == 'securite'):
+        echo $RssSecurite->channel->item[1]->title;
+    elseif (isset($_GET['page']) && $_GET['page'] == 'applis'):
+        echo $RssApplisLogiciels->channel->item[0]->title;
+    elseif (isset($_GET['page']) && $_GET['page'] == 'technos'):
+        echo $RssTechnos->channel->item[0]->title;
+    endif;
+
+    if (!empty($loginUtili) && !empty($mdpUtili) && !empty($choiceColor) && !empty($articleNumber)): ?>
+        <div class="row">
+            <div class="col m4">
+                <h1 class="titleHome" style="color: <?= $choiceColor ?>"><b><u>Sécurité</u></b></h1>
+            </div>
+            <div class="col m4">
+                <h1 class="titleHome" style="color: <?= $choiceColor ?>"><b><u>Applis/Logiciels</u></b></h1>
+            </div>
+            <div class="col m4">
+                <h1 class="titleHome" style="color: <?= $choiceColor ?>"><b><u>Technos</u></b></h1>
+            </div>
+        </div>
+    <?php
+    for ($i = 0; $i < $_GET['articleNumber']; $i++): 
+    setlocale (LC_TIME, 'fr_FR.utf8','fra'); ?>
+    <div class="row">
+        <div class="col m4">
+
+            <img src="<?= $RssSecurite->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100"
+                height="50">
+            <span class="title"><?= $RssSecurite->channel->item[$i]->title ?></span>
+            <p>Sécurité<br>
+                <?= $RssSecurite->channel->item[$i]->pubDate ?> <br>
+                <a class="waves-effect waves-light btn modal-trigger"
+                    href="<?= $RssSecurite->channel->item[$i]->link ?>">Lien</a>
+
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Description</a>
+                <div id="modal1" class="modal">
+                    <div class="modal-content">
+                        <h4>Description</h4>
+                        <p><?= $RssSecurite->channel->item[$i]->description ?></p>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="<?= $RssSecurite->channel->item[$i]->link ?>"
+                            class="waves-effect waves-green btn-flat">Lien</a>
+                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
+                    </div>
+                </div>
+            </p>
 
 
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </div>
+        <div class="col m4">
+            <img src="<?= $RssApplisLogiciels->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100"
+                height="50">
+            <span class="title"><?= $RssApplisLogiciels->channel->item[$i]->title ?></span>
+            <p>Applis/Logiciels <br>
+                <?= $RssApplisLogiciels->channel->item[$i]->pubDate ?> <br>
+                <a class="waves-effect waves-light btn modal-trigger"
+                    href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>">Lien</a>
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal2">Description</a>
+                <div id="modal2" class="modal">
+                    <div class="modal-content">
+                        <h4>Description</h4>
+                        <p><?= $RssApplisLogiciels->channel->item[$i]->description ?></p>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>"
+                            class="waves-effect waves-green btn-flat">Lien</a>
+                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
+                    </div>
+                </div>
+            </p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </div>
+        <div class="col m4">
+            <img src="<?= $RssTechnos->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100"
+                height="50">
+            <span class="title"><?= $RssTechnos->channel->item[$i]->title ?></span>
+            <p>Technos <br>
+                <?= $RssTechnos->channel->item[$i]->pubDate ?> <br>
+                <a class="waves-effect waves-light btn modal-trigger"
+                    href="<?= $RssTechnos->channel->item[$i]->link ?>">Lien</a>
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal3">Description</a>
+                <div id="modal3" class="modal">
+                    <div class="modal-content">
+                        <h4>Description</h4>
+                        <p><?= $RssTechnos->channel->item[$i]->description ?></p>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="<?= $RssTechnos->channel->item[$i]->link ?>"
+                            class="waves-effect waves-green btn-flat">Lien</a>
+                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
+                    </div>
+                </div>
+            </p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </div>
+    </div>
+    <?php
+endfor;
+    else: ?>
     <form method="get" action="index.php">
-        <p><label>Votre nom : </label><input type="text" name="lastName" /></p>
-        <p><label>Votre prénom : </label><input type="text" name="firstName" /></p>
+        <p><label>Votre nom : </label><input type="text" name="lastName" required /></p>
+        <p><label>Votre prénom : </label><input type="text" name="firstName" required /></p>
         <p>
             <label> Quelle est votre couleur préféré ?</label>
         </p>
@@ -104,76 +208,8 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
         <input type="submit" value="Valider" />
     </form>
     <?php
-    for ($i = 0; $i < $_GET['articleNumber']; $i++): 
-    setlocale (LC_TIME, 'fr_FR.utf8','fra'); ?>
-    <div class="row">
-        <div class="col m4">
-            <img src="<?= $RssSecurite->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100" height="50">
-            <span class="title"><?= $RssSecurite->channel->item[$i]->title ?></span>
-            <p>Sécurité<br>
-                <?= $RssSecurite->channel->item[$i]->pubDate ?> <br>
-                <a class="waves-effect waves-light btn modal-trigger" href="<?= $RssSecurite->channel->item[$i]->link ?>">Lien</a>
+    endif; ?>
 
-                <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Description</a>
-                <div id="modal1" class="modal">
-                    <div class="modal-content">
-                        <h4>Description</h4>
-                        <p><?= $RssSecurite->channel->item[$i]->description ?></p>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="<?= $RssSecurite->channel->item[$i]->link ?>" class="waves-effect waves-green btn-flat">Lien</a>
-                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
-                    </div>
-                </div>
-            </p>
-
-
-            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-        </div>
-        <div class="col m4">
-            <img src="<?= $RssApplisLogiciels->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100" height="50">
-            <span class="title"><?= $RssApplisLogiciels->channel->item[$i]->title ?></span>
-            <p>Applis/Logiciels <br>
-                <?= $RssApplisLogiciels->channel->item[$i]->pubDate ?> <br>
-                <a class="waves-effect waves-light btn modal-trigger" href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>">Lien</a>
-                <a class="waves-effect waves-light btn modal-trigger" href="#modal2">Description</a>
-                <div id="modal2" class="modal">
-                    <div class="modal-content">
-                        <h4>Description</h4>
-                        <p><?= $RssApplisLogiciels->channel->item[$i]->description ?></p>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>" class="waves-effect waves-green btn-flat">Lien</a>
-                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
-                    </div>
-                </div>
-            </p>
-            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-        </div>
-        <div class="col m4">
-            <img src="<?= $RssTechnos->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100" height="50">
-            <span class="title"><?= $RssTechnos->channel->item[$i]->title ?></span>
-            <p>Technos <br>
-                <?= $RssTechnos->channel->item[$i]->pubDate ?> <br>
-                <a class="waves-effect waves-light btn modal-trigger" href="<?= $RssTechnos->channel->item[$i]->link ?>">Lien</a>
-                <a class="waves-effect waves-light btn modal-trigger" href="#modal3">Description</a>
-                <div id="modal3" class="modal">
-                    <div class="modal-content">
-                        <h4>Description</h4>
-                        <p><?= $RssTechnos->channel->item[$i]->description ?></p>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="<?= $RssTechnos->channel->item[$i]->link ?>" class="waves-effect waves-green btn-flat">Lien</a>
-                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
-                    </div>
-                </div>
-            </p>
-            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-        </div>
-    </div>
-    <?php
-endfor;
-?>
 
     <style type="text/css">
         .color {
