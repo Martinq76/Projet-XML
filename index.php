61452819c39af4be9d621e669405222d6bda073a
Martinq76 <martin.queguiner.42@gmail.com>
2019-06-12 22:49:48 +0200

    CSS OK

--- index.php
+++ index.php
@@ -13,15 +13,20 @@ if (!empty($_GET['lastName']) && !empty($_GET['firstName']) && !empty($_GET['cho
 }
 
 if (isset($_GET) && $_GET['page'] == 'deconnexion'):
-    unset($_COOKIE['lastName']);
-    unset($_COOKIE['firstName']);
-    unset($_COOKIE['articleNumber']);
-    unset($_COOKIE['choiceColor']);
+    setcookie('Bonjour', 'Salut', time() + 360, '/', 'rssfeed.info', false, true);
+    echo $_COOKIE['Bonjour'];
+    setcookie('Bonjour', FALSE, time() - 3600, '/', 'rssfeed.info', false, true);
+    unset($_COOKIE['Bonjour']);
 endif;
 
 $XmlSecurite = "https://www.01net.com/rss/actualites/securite/";
 $XmlApplisLogiciels = "https://www.01net.com/rss/actualites/applis-logiciels/";
 $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
+
+setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
+function dateFr($date) {
+    return ucfirst(strftime("%A %d %B %Y %H:%M:%S", strtotime($date)));
+}
 ?>
 
 <!DOCTYPE html>
@@ -44,21 +49,24 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                     if ($_COOKIE['choiceColor'] == 'black'):
                         echo 'black';
                     elseif ($_COOKIE['choiceColor'] == 'blue'):
-                        echo 'blue';
+                        echo 'cyan darken-3';
                     elseif ($_COOKIE['choiceColor'] == 'red'):
-                        echo 'red';
+                        echo 'red darken-2';
                     elseif (count($_COOKIE) == 0):
                         echo 'red';
                     endif; ?>">
         <div class="nav-wrapper">
-            <a href="/" class="brand-logo toto">Logoo</a>
+            <a href="accueil" class="brand-logo toto">Logoo</a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="securite">Sécurité</a></li>
                 <li><a href="applis">Application</a></li>
                 <li><a href="technos">Technos</a></li>
-                <li><?= $_COOKIE['firstName'] ?></li>
-                <li><a class="waves-effect waves-light btn modal-trigger" href="#inscription">Inscription</a></li>
+                <li style="font-weight:bold; font-size:1.5rem; color:#616161"> <?php
+                if (!empty($_COOKIE['firstName'])):?>
+                Bonjour <?= $_COOKIE['firstName'] ?></li> <?php
+                endif; ?>
+                <li><a class="waves-effect grey lighten-5 btn modal-trigger" style="color:black" href="#inscription">Inscription</a></li>
                 <li><a href="deconnexion">Déconnexion</a></li>
                 <li><a href="#parametres" class="modal-trigger"><i class="material-icons"> settings</i></a></li>
             </ul>
@@ -68,7 +76,7 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
         <li><a href="securite">Sécurité</a></li>
         <li><a href="applis">Application</a></li>
         <li><a href="technos">Technos</a></li>
-        <li><?= $_GET['firstName'] ?></li>
+        <li><b><?= $_GET['firstName'] ?></b></li>
         <li><a class="waves-effect waves-light btn modal-trigger" href="#inscription">Inscription</a></li>
         <li><a href="deconnexion">Déconnexion</a></li>
         <li><a href="#parametres" class="modal-trigger"><i class="material-icons"> settings</i></a></li>
@@ -178,6 +186,8 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
     </div>
 
     <?php
+if (!empty($_COOKIE['firstName']) && !empty($_COOKIE['lastName']) && !empty($_COOKIE['choiceColor']) && !empty($_COOKIE['articleNumber'])): 
+
     if (isset($_GET['page']) && $_GET['page'] == 'securite'): ?>
     <div class="row">
         <?php
@@ -188,15 +198,16 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                 <div class="card-image">
                     <img src="<?= $RssSecurite->channel->item[$i]->enclosure{'url'} ?>" alt="" width="50" height="250">
                     <span class="card-title"
-                        style="color: <?= $_COOKIE['choiceColor']; ?>"><?= $RssSecurite->channel->item[$i]->title ?></span>
+                    style="font-weight: bold; color: <?= $_COOKIE['choiceColor']; ?> ;text-shadow: -1px 0 white, 0 1px white,
+                    1px 0 white, 0 -1px white"><?= $RssSecurite->channel->item[$i]->title ?></span>
                 </div>
                 <div class="card-content">
-                    <p> <?= $RssSecurite->channel->item[$i]->pubDate ?> </p>
+                    <p> <?= dateFr($RssSecurite->channel->item[$i]->pubDate); ?> </p>
                 </div>
                 <div class="card-action">
-                    <a class="waves-effect waves-light btn modal-trigger"
+                    <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
                         href="<?= $RssSecurite->channel->item[$i]->link ?>">Lien vers l'article</a>
-                    <a class="waves-effect waves-light btn modal-trigger"
+                    <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
                         href="#descriptionCategorie1<?= $i ?>">Description</a>
                     <div id="descriptionCategorie1<?= $i ?>" class="modal">
                         <div class="modal-content">
@@ -212,7 +223,6 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                 </div>
             </div>
         </div>
-
         <?php endfor; ?>
     </div>
     <?php 
@@ -226,16 +236,17 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                 <div class="card-image">
                     <img src="<?= $RssApplisLogiciels->channel->item[$i]->enclosure{'url'} ?>" alt="" width="50"
                         height="250">
-                    <span class="card-title"
-                        style="color: <?= $_COOKIE['choiceColor']; ?>"><?= $RssApplisLogiciels->channel->item[$i]->title ?></span>
+                        <span class="card-title"
+                        style="font-weight: bold; color: <?= $_COOKIE['choiceColor']; ?> ;text-shadow: -1px 0 white, 0 1px white,
+                        1px 0 white, 0 -1px white"><?= $RssApplisLogiciels->channel->item[$i]->title ?></span>
                 </div>
                 <div class="card-content">
-                    <p> <?= $RssApplisLogiciels->channel->item[$i]->pubDate ?> </p>
+                    <p> <?= dateFr($RssApplisLogiciels->channel->item[$i]->pubDate); ?> </p>
                 </div>
                 <div class="card-action">
-                    <a class="waves-effect waves-light btn modal-trigger"
+                    <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
                         href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>">Lien vers l'article</a>
-                    <a class="waves-effect waves-light btn modal-trigger"
+                    <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
                         href="#descriptionCategorie2<?= $i ?>">Description</a>
                     <div id="descriptionCategorie2<?= $i ?>" class="modal">
                         <div class="modal-content">
@@ -251,7 +262,6 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                 </div>
             </div>
         </div>
-
         <?php
         endfor; ?>
     </div>
@@ -259,23 +269,23 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
     elseif (isset($_GET['page']) && $_GET['page'] == 'technos'): ?>
     <div class="row">
         <?php
-                            for ($i = 0; $i <= 29; $i++):
-                                ?>
-
+        for ($i = 0; $i <= 29; $i++):
+        ?>
         <div class="col s12 m4">
             <div class="card">
                 <div class="card-image">
                     <img src="<?= $RssTechnos->channel->item[$i]->enclosure{'url'} ?>" alt="" width="50" height="250">
                     <span class="card-title"
-                        style="color: <?= $_COOKIE['choiceColor']; ?>"><?= $RssTechnos->channel->item[$i]->title ?></span>
+                    style="font-weight: bold; color: <?= $_COOKIE['choiceColor']; ?> ;text-shadow: -1px 0 white, 0 1px white,
+                    1px 0 white, 0 -1px white"><?= $RssTechnos->channel->item[$i]->title ?></span>
                 </div>
                 <div class="card-content">
-                    <p> <?= $RssTechnos->channel->item[$i]->pubDate ?> </p>
+                    <p> <?= dateFr($RssTechnos->channel->item[$i]->pubDate); ?> </p>
                 </div>
                 <div class="card-action">
-                    <a class="waves-effect waves-light btn modal-trigger"
+                    <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
                         href="<?= $RssTechnos->channel->item[$i]->link ?>">Lien vers l'article</a>
-                    <a class="waves-effect waves-light btn modal-trigger"
+                    <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
                         href="#descriptionCategorie3<?= $i ?>">Description</a>
                     <div id="descriptionCategorie3<?= $i ?>" class="modal">
                         <div class="modal-content">
@@ -294,10 +304,8 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
         <?php endfor; ?>
     </div>
     <?php
-    endif;
-
-    if (!empty($_COOKIE['firstName']) && !empty($_COOKIE['lastName']) && !empty($_COOKIE['choiceColor']) && !empty($_COOKIE['articleNumber'])): ?>
-    <div class="row">
+    elseif (count($_GET) == 0 || isset($_GET['page']) && $_GET['page'] == 'accueil'): ?>
+        <div class="row">
         <div class="col m4">
             <h1 class="titleHome" style="color: <?= $_COOKIE['choiceColor'] ?>"><b><u>Sécurité</u></b></h1>
         </div>
@@ -315,13 +323,15 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
         <div class="col m4">
             <img src="<?= $RssSecurite->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100"
                 height="50">
-            <span class="title"><?= $RssSecurite->channel->item[$i]->title ?></span>
+                <p class="title" style="font-weight: bold; color: <?= $_COOKIE['choiceColor']; ?> ;text-shadow: -1px 0 white, 0 1px white,
+                1px 0 white, 0 -1px white"><?= $RssSecurite->channel->item[$i]->title ?></p>
             <p>Sécurité<br>
-                <?= $RssSecurite->channel->item[$i]->pubDate ?> <br>
-                <a class="waves-effect waves-light btn modal-trigger"
-                    href="<?= $RssSecurite->channel->item[$i]->link ?>">Lien</a>
+                <?= dateFr($RssSecurite->channel->item[$i]->pubDate); ?> <br>
+                <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
+                    href="<?= $RssSecurite->channel->item[$i]->link ?>">Lien vers l'article</a>
 
-                <a class="waves-effect waves-light btn modal-trigger" href="#modalHomeCol1<?= $i ?>">Description</a>
+                <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>"
+                    href="#modalHomeCol1<?= $i ?>">Description</a>
                 <div id="modalHomeCol1<?= $i ?>" class="modal">
                     <div class="modal-content">
                         <h4>Description</h4>
@@ -329,24 +339,23 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                     </div>
                     <div class="modal-footer">
                         <a href="<?= $RssSecurite->channel->item[$i]->link ?>"
-                            class="waves-effect waves-green btn-flat">Lien</a>
+                            class="waves-effect btn-flat" style="background-color: <?= $_COOKIE['choiceColor']; ?>">Lien vers l'article</a>
                         <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
                     </div>
                 </div>
             </p>
-
-
-            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
         </div>
         <div class="col m4">
             <img src="<?= $RssApplisLogiciels->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100"
                 height="50">
-            <span class="title"><?= $RssApplisLogiciels->channel->item[$i]->title ?></span>
+                <p class="title" style="font-weight: bold; color: <?= $_COOKIE['choiceColor']; ?> ;text-shadow: -1px 0 white, 0 1px white,
+                1px 0 white, 0 -1px white"><?= $RssApplisLogiciels->channel->item[$i]->title ?></p>
             <p>Applis/Logiciels <br>
-                <?= $RssApplisLogiciels->channel->item[$i]->pubDate ?> <br>
-                <a class="waves-effect waves-light btn modal-trigger"
-                    href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>">Lien</a>
-                <a class="waves-effect waves-light btn modal-trigger" href="#modalHomeCol2<?= $i ?>">Description</a>
+                <?= dateFr($RssApplisLogiciels->channel->item[$i]->pubDate); ?> <br>
+                <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>"
+                    href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>">Lien vers l'article</a>
+                <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
+                    href="#modalHomeCol2<?= $i ?>">Description</a>
                 <div id="modalHomeCol2<?= $i ?>" class="modal">
                     <div class="modal-content">
                         <h4>Description</h4>
@@ -354,22 +363,23 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                     </div>
                     <div class="modal-footer">
                         <a href="<?= $RssApplisLogiciels->channel->item[$i]->link ?>"
-                            class="waves-effect waves-green btn-flat">Lien</a>
+                        class="waves-effect btn-flat" style="background-color: <?= $_COOKIE['choiceColor']; ?>">Lien vers l'article</a>
                         <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
                     </div>
                 </div>
             </p>
-            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
         </div>
         <div class="col m4">
             <img src="<?= $RssTechnos->channel->item[$i]->enclosure{'url'} ?>" alt="" class="circle" width="100"
                 height="50">
-            <span class="title"><?= $RssTechnos->channel->item[$i]->title ?></span>
+                <p class="title" style="font-weight: bold; color: <?= $_COOKIE['choiceColor']; ?> ;text-shadow: -1px 0 white, 0 1px white,
+                1px 0 white, 0 -1px white"><?= $RssTechnos->channel->item[$i]->title ?></p>
             <p>Technos <br>
-                <?= $RssTechnos->channel->item[$i]->pubDate ?> <br>
-                <a class="waves-effect waves-light btn modal-trigger"
-                    href="<?= $RssTechnos->channel->item[$i]->link ?>">Lien</a>
-                <a class="waves-effect waves-light btn modal-trigger" href="#modalHomeCol3<?= $i ?>">Description</a>
+                <?= dateFr($RssTechnos->channel->item[$i]->pubDate); ?> <br>
+                <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>"
+                    href="<?= $RssTechnos->channel->item[$i]->link ?>">Lien vers l'article</a>
+                <a class="waves-effect waves-light btn modal-trigger" style="background-color: <?= $_COOKIE['choiceColor']; ?>" 
+                    href="#modalHomeCol3<?= $i ?>">Description</a>
                 <div id="modalHomeCol3<?= $i ?>" class="modal">
                     <div class="modal-content">
                         <h4>Description</h4>
@@ -377,17 +387,19 @@ $XmlTechnos = "https://www.01net.com/rss/actualites/technos/";
                     </div>
                     <div class="modal-footer">
                         <a href="<?= $RssTechnos->channel->item[$i]->link ?>"
-                            class="waves-effect waves-green btn-flat">Lien</a>
+                        class="waves-effect btn-flat" style="background-color: <?= $_COOKIE['choiceColor']; ?>">Lien vers l'article</a>
                         <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
                     </div>
                 </div>
             </p>
-            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
         </div>
     </div>
     <?php
 endfor;
-    endif; ?>
+endif;
+endif; ?>
+
+    
 
 
     <style type="text/css">
